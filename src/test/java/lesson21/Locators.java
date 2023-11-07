468d819a29af09d3b189ee1b376155ec8dfd20a9
KaterynaArt <artusenkoekaterina3@gmail.com>
2023-11-07 19:43:21 +0100

    lesson21

---
+++ src/test/java/lesson21/Locators.java
@@ -0,0 +1,34 @@
+package lesson21;
+
+public class Locators {
+
+    public static void main(String[] args) {
+        //  By ID
+        //      <input type="text" id="uniqueId">
+        //        WebElement element = driver.findElement(By.id("uniqueId"));
+
+        // By Name
+         //     <input type="text" name"username">
+        //      <input type="text" name"password">
+        //      WebElement element = driver.findElement(By.name("username"));
+
+        // By class Name
+        //      <button class="btn-primary">Надicлати</button>
+       //       <button class="btn-secondary">Cкасувати</button>
+
+        // By Tag Name
+       //       <a href= "https://www.example.com/">Посилання</a>
+       //        <input type="text" name"username">
+        //         WebElement element = driver.findElement(By.tagName("a");
+
+
+        // By Link Text
+        //       <a href= "https://www.example.com/">Посилання на приклад</a>
+        //         WebElement element = driver.findElement(By.linkText("Посилання на приклад");
+
+        // Partial Link Text
+        //       <a href= "https://www.example.com/">Посилання на приклад</a>
+       //       WebElement element = driver.findElement(By.partialLinkText(" на приклад");
+    }
+}
+
---
+++ src/test/java/lesson21/WebDriverInteraction.java
@@ -0,0 +1,33 @@
+package lesson21;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import java.util.List;
+public class WebDriverInteraction {
+
+        private WebDriver driver;
+
+        @BeforeTest
+        public void initDriver(){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+    public void example() {
+        WebElement inputSearch = driver.findElement(By.cssSelector(".someInputClass"));
+        WebElement btnSearch = driver.findElement(By.xpath("//div[@btnSearch]"));
+
+        List<WebElement> elementList = (List<WebElement>) driver.findElement(By.cssSelector(".myClass"));
+
+       inputSearch.sendKeys("Some text");
+       btnSearch.click();
+
+       String colorBtn = btnSearch.getAttribute("color");
+       String btnText = btnSearch.getText();
+
+       boolean inDisplayBtnSearch = btnSearch.isDisplayed();
+    }
+}
