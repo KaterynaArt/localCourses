b8514f6fd777a22ebc0176e733016785ca152bf2
KaterynaArt <artusenkoekaterina3@gmail.com>
2023-11-14 21:01:44 +0100

    Lesson23

--- src/test/java/init/WebDriverInit.java
+++ src/test/java/init/WebDriverInit.java
@@ -3,7 +3,6 @@ package init;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -17,9 +16,9 @@ public class WebDriverInit {
     @BeforeTest
     public void initDriver() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(new ChromeOptions().addArguments("--start-maximized"));
+        driver = new ChromeDriver();
         webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
     }
 
     @AfterTest
