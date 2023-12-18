28de24249a99ad53ab92e2992d756ad511d25add
KaterynaArt <artusenkoekaterina3@gmail.com>
2023-12-18 10:27:47 +0100

    lesson_31

---
+++ src/test/java/lesson31/registration/UnSuccessUserReg.java
@@ -0,0 +1,19 @@
+package lesson31.registration;
+
+public class UnSuccessUserReg {
+
+
+    private String error;
+
+    public UnSuccessUserReg() {
+
+    }
+
+    public UnSuccessUserReg(String error) {
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
+    }
+}
